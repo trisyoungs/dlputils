4d75297ef670ac8fbe6d9ad8e2d461c15c49f519
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@de35bcdd-ed9d-7249-3847-1726e8208217>
2010-06-10 13:09:14 +0000

    Added ability for filterdlp.f90 to accept numerical frame range instead of relying on a file.

--- trunk/filterdlp.f90
+++ trunk/filterdlp.f90
@@ -5,22 +5,36 @@
 	program filterdlp
 	use dlprw
 	implicit none
-	character*80 :: hisfile,outfile,framefile
+	character*80 :: hisfile,outfile,framefile,temp
 	integer :: nargs,n,i,m,o,success,nframes,seek,nfiltered
-	integer :: iargc
+	integer :: iargc, framefirst, framelast, frameskip
+	logical :: fromfile = .true.
 
 	nargs = iargc()
-	if (nargs.NE.3) stop "Usage : filterdlp <input HISfile> <output HISfile> <frame file>"
+	if ((nargs.ne.3).and.(nargs.ne.5)) stop "Usage : filterdlp <input HISfile> <output HISfile> <frame file | first skip last>"
 	call getarg(1,hisfile)
 	call getarg(2,outfile)
-	call getarg(3,framefile)
+	if (nargs.eq.3) then
+	  call getarg(3,framefile)
+	  write(6,*) "Using frames listed in supplied file...."
+	else
+	  call getarg(3,temp); read(temp,"(i7)") framefirst
+	  call getarg(4,temp); read(temp,"(i7)") frameskip
+	  call getarg(5,temp); read(temp,"(i7)") framelast
+	  write(6,"(a,i6,a,i6,a,i6)") "Using frame range ",framefirst," to ",framelast," with a skip of ", frameskip
+	  fromfile = .false.
+	end if
 
 	write(6,*) "Filtering..."
 	nfiltered = 0
 
-	! Open the frames file and read in the first target
-	open(unit=19,file=framefile,form="formatted",status="old")
-	read(19,"(I10)") seek
+	! Open the frames file and read in the first target (if using a file)
+	if (fromfile) then
+	  open(unit=19,file=framefile,form="formatted",status="old")
+	  read(19,"(I10)") seek
+	else
+	  seek = framefirst
+	end if
 
 	nframes = 0
 	! Open the file and read in the file header (not used)
@@ -34,7 +48,7 @@
 
 10	success = readframe()
 	if (success.NE.0) then
-	  write(0,"(A,A)") "Failed to read first frame of file."
+	  write(0,"(A,i6,A)") "Failed to read frame ", nframes," from file"
 	  stop "Quit."
 	end if
 	nframes = nframes + 1
@@ -43,8 +57,14 @@
 	  success = writeframe()
 	  if (success.EQ.-1) stop "Failed to write frame!"
 	  nfiltered = nfiltered + 1
-	  ! Read in the next target frame
-	  read(19,"(I10)",err=17,end=14) seek
+
+	  ! Read in the next target frame (if using a file)
+	  if (fromfile) then
+	    read(19,"(I10)",err=17,end=14) seek
+	  else
+	    seek = seek + frameskip
+	    if (seek.gt.framelast) goto 15
+	  end if
 	  write(0,"(A,I6,A)") "Seeking frame ",seek,"..."
 	end if
 	goto 10
