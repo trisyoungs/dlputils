6743770e4c93f454ccc21711002e06675fd3d76b
trisyoungs@googlemail.com <trisyoungs@googlemail.com@de35bcdd-ed9d-7249-3847-1726e8208217>
2014-04-01 14:30:36 +0000

    Updated xyz2his to have better error reporting, use proper command-line arguments, and allow optional skipping of blank line in between frames in input file.

--- trunk/intertorsion.f90
+++ trunk/intertorsion.f90
@@ -24,7 +24,7 @@
 
 	nargs = iargc()
 	if (nargs.lt.5) then
-	  write(0,*) "Usage : torsion <DLP HISTORYfile> <DLP OUTPUTfile> <species> <atom1> <atom2> [jj cutoff]"
+	  write(0,*) "Usage : intertorsion <DLP HISTORYfile> <DLP OUTPUTfile> <species> <atom1> <atom2> [jj cutoff]"
 	  write(0,*) "             --- Torsion angle is i1-j1-j2-i2 (i-j-k-l)"
 	  stop
 	end if
