150e327ff8d7df1348f89d186a53696a313087b8
trisyoungs@googlemail.com <trisyoungs@googlemail.com@de35bcdd-ed9d-7249-3847-1726e8208217>
2015-04-02 10:34:23 +0000

    Moved IntegerList to its own module, and rejigged codes to use this instead.

--- trunk/parse.f90
+++ trunk/parse.f90
@@ -6,12 +6,6 @@
 	  integer :: nargsparsed
 	  character(MAXARGLEN) :: arg(MAXARGS)
 
-	  ! Integer list
-	  integer, parameter :: MAXLISTITEMS = 20
-	  type IntegerList
-	    integer :: n = 0
-	    integer, dimension(MAXLISTITEMS) :: items = 0
-	  end type IntegerList
 	contains
 
 	! Read line from specified unit number and split into delimited arguments
@@ -82,59 +76,6 @@
 	  read(arg(n),"(f30.10)") argr
 	end if
 	end function argr
-
-	! Parse string as comma-separated integer list into array (with index 0 containing the number of items)
-	logical function parseIntegerList(string, list)
-	implicit none
-	character*(*) :: string
-	type(IntegerList), intent(inout) :: list
-	integer :: n, arglen, slen
-	character :: i(MAXARGLEN)
-
-	! Now get length of supplied string argument
-	slen = len(string)
-	!write(0,*) "String length = ", slen
-	
-	! Step through specified argument, looking for commas
-	parseIntegerList = .true.
-	arglen = 0
-	i(:) = " "
-	do n=1,slen
-	  select case (ichar(string(n:n)))
-	    case (44,32)
-	      ! Comma (44) or space (32)
-	      ! If arglen != 0 then 'store' current argument.
-	      ! Otherwise, move on
-	      if (arglen.ne.0) then
-		list%n = list%n + 1
-		if (list%n.gt.MAXLISTITEMS) then
-		  write(0,*) "Integer list maximum size exceeded. MAXITEMS = ", MAXLISTITEMS
-		  parseIntegerList = .false.
-		  return
-		end if
-		read(i, "(i30)") list%items(list%n)
-		arglen = 0
-		i(:) = " "
-		cycle
-	      else if (ichar(string(n:n)).eq.44) then
-		write(0,*) "!!! Found comma at start of argument?"
-	        parseIntegerList = .false.
-	        return
-	      end if
-	      exit
-	    case (48:57)
-	      ! Number - add to argument and continue
-	      arglen = arglen + 1
-	      i(arglen:arglen) = string(n:n)
-	    case default
-	      ! Not a comma, not a space, and not a number, so complain
-	      write(0,*) "Found illegal character in integer list: ", string(n:n), " at pos ", n
-	      parseIntegerList = .false.
-	      return
-	  end select
-	end do
-
-	end function parseIntegerList
 
 	! Retrieve CLI argument specified as a floating-point number
 	real*8 function getargr(argindex)
